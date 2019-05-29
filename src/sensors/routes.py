0c7f4378ebc61a161c4f975c2e3e750ad81d2e97
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-28 23:13:25 -0300

    Create route for signup

--- src/sensors/routes.py
+++ src/sensors/routes.py
@@ -1,9 +1,22 @@
 from boogie.router import Router
+from django.http import Http404, HttpResponse
+from .models import User
 
 urlpatterns = Router()
 
-@urlpatterns.route("")
-def detail(request):
-    user = request.user
-    return {"user": user
-    }
+@urlpatterns.route("signup/")
+def signup(request):
+    if(request.method == "POST"):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if username and password:
+            user = User.objects.create_user(username, password)
+            response = {"authentication_token": user.authToken}
+            return HttpResponse(str(response))
+        else:
+            return Http404("You need to send password and username.")
+    return Http404("Can't signup with GET method.")
+
+@urlpatterns.route("login/")
+def login(request):
+    return {}
