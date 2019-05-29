85ba7def3c968e7829f719714707b0f71dd15ae0
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-29 14:29:14 -0300

    Fix returns on wrong requests for signup

--- src/sensors/routes.py
+++ src/sensors/routes.py
@@ -1,21 +1,29 @@
 from boogie.router import Router
 from django.http import Http404, HttpResponse
 from .models import User
+from rest_framework import status
 
 urlpatterns = Router()
 
 @urlpatterns.route("signup/")
 def signup(request):
+    rstatus = status.HTTP_403_FORBIDDEN
     if(request.method == "POST"):
-        username = request.POST["username"]
-        password = request.POST["password"]
-        if username and password:
-            user = User.objects.create_user(username, password)
+        if request.POST.keys() >= {"username", "password"}:
+            username = request.POST['username']
+            password = request.POST['password']
+            try:
+                user = User.objects.create_user(username, password)
+            except:
+                response = {"Unavailable username": "username already taken"}
             response = {"authentication_token": user.authToken}
-            return HttpResponse(str(response))
+            rstatus = status.HTTP_200_OK
         else:
-            return Http404("You need to send password and username.")
-    return Http404("Can't signup with GET method.")
+            response = {"Not enough credencials": "You need to send password and username."}
+    else:
+        response = {"Wrong method.": "Can't signup with GET method."}
+
+    return HttpResponse(str(response), status=rstatus)
 
 @urlpatterns.route("login/")
 def login(request):
