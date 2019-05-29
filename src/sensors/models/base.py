5cbe5e87f70e190a92ca678480b908c2b7602de3
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-29 15:44:36 -0300

    Expose data api routes

--- src/sensors/models/base.py
+++ src/sensors/models/base.py
@@ -2,21 +2,20 @@ from django.db import models
 from boogie.rest import rest_api
 from ..manager import UserManager
 
-@rest_api(['username'])
+@rest_api(exclude=['hash','authToken'])
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     hash = models.CharField(max_length=128)
     authToken = models.CharField(max_length=64)
     objects = UserManager()
-       
 
 @rest_api()
 class DataCluster(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="data")
     collected_at = models.DateTimeField(auto_now=True)
 
 class Data(models.Model):
-    data_cluster = models.ForeignKey(DataCluster, on_delete=models.CASCADE)
+    data_cluster = models.ForeignKey(DataCluster, on_delete=models.CASCADE, related_name="%(class)s")
 
     class Meta:
         abstract = True
--- src/sensors/models/data.py
+++ src/sensors/models/data.py
@@ -1,31 +1,32 @@
 from django.db import models
+from boogie.rest import rest_api
 
 from . import base
 
-
+@rest_api()
 class SoilData(base.Data):
     moisture = models.FloatField()
     temperature = models.FloatField()
 
-
+@rest_api()
 class AirData(base.Data):
     humidity = models.FloatField()
     temperature = models.FloatField()
     pressure = models.FloatField()
 
-
+@rest_api()
 class WindData(base.Data):
     speed = models.FloatField()
     direction = models.PositiveIntegerField()
 
-
+@rest_api()
 class RainData(base.Data):
     rainfall = models.PositiveIntegerField()
 
-
+@rest_api()
 class SolarData(base.Data):
     radiation = models.PositiveIntegerField()
 
-
+@rest_api()
 class ActuatorData(base.Data):
     status = models.BooleanField(default=False)
