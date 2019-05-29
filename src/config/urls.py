6c7f76b6c9e25f151347b67dfe8eef0eb7a5a65a
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-28 21:53:39 -0300

    Remove unused views and urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from sensors.urls import router_v1
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router_v1.urls))
+    path('', include('sensor.routes'))
 ]
