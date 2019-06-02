894858376e5fb07aefcdab32de0642e65a962aa7
pablodiegoss <pablodiegoss@hotmail.com>
2019-06-02 19:28:56 -0300

    Update send_data method to json

--- src/sensors/routes.py
+++ src/sensors/routes.py
@@ -60,43 +60,49 @@ def login(request):
 
 @urlpatterns.route('send-data/')
 def send_data(request):
-    if(request.method == "POST"):
-        data = json.loads(request.body)
-        if 'auth_token' in data.keys():
-            if 'data' in data.keys():
-                user = User.objects.filter(auth_token=data['auth_token'])
-                if user:
-                    # try:
-                    create_data(user.first(), json.loads(request.POST['data']))
-                    return HttpResponse("Ae", status=status.HTTP_201_CREATED)
-                    # except:
-                        # return HttpResponse("Data in invalid format", status=status.HTTP_406_NOT_ACCEPTABLE)
-                else:
-                    return HttpResponse("Unauthorized.", status=status.HTTP_403_FORBIDDEN)
-            else:
-                return HttpResponse("No data.", status=status.HTTP_403_FORBIDDEN)
+    user = verify_auth(request)
+    data = get_data(request)
+    print(data)
+    if user and data:
+        # try:
+        create_data(user, data)
+        return HttpResponse("Ae", status=status.HTTP_201_CREATED)
+        # except:
+            # return HttpResponse("Data in invalid format", status=status.HTTP_406_NOT_ACCEPTABLE)
+    else:
         return HttpResponse("Unauthorized.", status=status.HTTP_403_FORBIDDEN)
-    return HttpResponse("Wrong http method, not a POST!", status=status.HTTP_400_BAD_REQUEST)
 
-
-def create_data(user, request_data):
+def create_data(user, cluster_data):
     cluster = DataCluster(owner=user)
     cluster.save()
-    print("Received data for: " + str(request_data.keys()))
-    for data in request_data.keys():
+    print("Received data for: " + str(cluster_data.keys()))
+    for data in cluster_data.keys():
         serializer = create_serializer(data)
-        serializer = serializer(data=request_data[data])
+        serializer = serializer(data=cluster_data[data])
         if serializer.is_valid():
             model = json_names[data]['model'](**serializer.data)
             model.data_cluster = cluster
             model.save()
 
-def verify_sent_credentials(request):
+def verify_auth(request):
     if(request.method == "POST"):
-        if request.POST.keys() >= {"username", "password"}:
-            return True
-    return False
+        data = json.loads(request.body)
+        if 'auth_token' in data.keys():
+            user = User.objects.filter(auth_token=data['auth_token'])
+            print(user)
+            if user:
+                return user.first()
+        return None
+    else:
+        return None
+
+def get_data(request):
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        if 'data' in data.keys():
+            return data['data']
+        return None
+    else:
+        return None
 
 
-
-    return pwdhash == stored_password
